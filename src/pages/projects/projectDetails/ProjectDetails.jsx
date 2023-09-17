e30bb2c662b2dcdc28c72f97494651ba93aea57c
Jahongir <nusratovjahongir@gmail.com>
2023-09-17 12:20:17 +0500

    add: some functionality to project details page

--- src/pages/projects/projectDetails/ProjectDetails.jsx
+++ src/pages/projects/projectDetails/ProjectDetails.jsx
@@ -1,19 +1,53 @@
-import React from "react"
 import { useParams } from "react-router-dom"
-import data from "../../../assets/data.json"
+import allProjects from "../../../assets/data.json"
 import "./projectDetails.scss"
+
 import { ImageSlider } from "../../../components/slider/ImageSlider"
+import Footer from "../../../components/footer/Footer"
+import House from "./House"
 
 function ProjectDetails() {
   const { id } = useParams()
 
-  const filteredData = data.projects.filter(
+  const filteredData = allProjects.projects.filter(
     (project) => project.projectId === parseInt(id)
   )
+  const data = filteredData[0]
 
   return (
     <section className="project-details">
-      <ImageSlider slides={filteredData[0].projectImgUrl} />
+      <ImageSlider slides={data.projectImgUrl} />
+      <div className="wrapper">
+        <h2>{data.projectArea}</h2>
+        <div>
+          <p>
+            Статус: <span> {data.projectStatus}</span>
+          </p>
+          <p>
+            Тип: <span> {data.projectType}</span>
+          </p>
+          <p>
+            Примечание: <span> {data.projectNote}</span>
+          </p>
+        </div>
+        <p>
+          Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру
+          сгенерировать несколько абзацев более менее осмысленного текста.
+          Вебмастеру сгенерировать несколько абзацев более менее осмысленного
+          текста.
+        </p>
+        <button className="btn">Подробнее</button>
+      </div>
+      <div className="house-details">
+        <div className="wrapper">
+          <h2>Площади и цены</h2>
+
+          {data.projects.map((project, index) => (
+            <House key={index} project={project} />
+          ))}
+        </div>
+      </div>
+      <Footer />
     </section>
   )
 }
--- src/pages/projects/projectsSection2/ProjectsSection2.jsx
+++ src/pages/projects/projectsSection2/ProjectsSection2.jsx
@@ -1,74 +1,13 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
 import "./projectsSection2.scss"
 import allProjects from "../../../assets/data.json"
+import Project from "./Project"
 
 export const ProjectsSection2 = () => {
-  const [extendContent, setExtendContent] = useState(false)
-
   return (
     <section className="project-section2">
       <div className="wrapper">
         {allProjects.projects.map((project, index) => (
-          <div key={index} className="projects-row flex">
-            <Link to={`/projects/${project.projectId}`}>
-              <img
-                src={project.projectImgUrl[0]}
-                alt={`project ${project.projectId}`}
-              />
-            </Link>
-
-            <div className="projects-content">
-              <Link to={`/projects/${project.projectId}`}>
-                <h2>{project.projectArea}</h2>
-              </Link>
-              <p>
-                Статус: <span>{project.projectStatus}</span>
-              </p>
-              <p>
-                Тип: <span>{project.projectType}</span>
-              </p>
-              <p>
-                Примечание: <span>{project.projectNote}</span>
-              </p>
-              <div
-                className={`table-content ${
-                  extendContent && "extended-content"
-                }`}
-              >
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Тип</th>
-                      <th>Спальни</th>
-                      <th>Плошадь</th>
-                      <th>Участок</th>
-                      <th>Цена</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {project.projects.map((item, index) => (
-                      <tr key={index}>
-                        <td>{item.type}</td>
-                        <td>{item.bedroom}</td>
-                        <td>{item.area} m2</td>
-                        <td>{item.yard} m2</td>
-                        <td>€{item.price}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-              {project.projects.length > 4 ? (
-                <button
-                  onClick={() => setExtendContent((prevState) => !prevState)}
-                  className="btn extend-btn"
-                >
-                  {extendContent ? "Завернуть" : "Развернуть"}
-                </button>
-              ) : null}
-            </div>
-          </div>
+          <Project key={index} project={project} />
         ))}
         <div className="projects-pagination">
           <button className="caret-left"></button>
